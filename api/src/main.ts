e97ac7a5b01400d81a80d70342d64ab3bcf6c545
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2026-01-10 19:07:03 +0200

    fix(api): make dto work by implementing global validation pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.FRONTEND_URL });
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
